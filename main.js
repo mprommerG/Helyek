45342bc862c833addce2b04981652bbfcc0aa1ee
mprommerG <96263424+mprommerG@users.noreply.github.com>
2021-12-16 15:48:41 -0500

    Upload map files

---
+++ main.js
@@ -0,0 +1,45 @@
+//function to start the Leaflet map
+function createMap(){
+
+    //Varibles for attribution box on the map.
+    var mbAttr = '<a href="http://openstreetmap.org">OpenStreetMap</a> |' +' <a href="http://mapbox.com">Mapbox</a>'; //You can add you name to the variable if you want to add yourself to the credits.
+    
+    //Variable for storing your Mapbox API Token
+    var apitoken = 'pk.eyJ1IjoibXByb21tZXIiLCJhIjoiY2t3MDFyMTlpMHgzODJxcDR0NHhlbGFnZSJ9.aPsdyoQ69RfsyAL_lZD8BA&zoomwheel=true&fresh=true#15.02/47.42231/19.02986' //Enter your API Token - go to 'https://www.mapbox.com/install/' to yours */
+    
+     //URL used for Stanard MaxBox Styles
+    var mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={token}';
+    
+    //URL used for Custom MapBox Style
+    var mbStyleUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/256/{z}/{x}/{y}?access_token={token}';
+    
+    //For Custome basemaps - Replace your username and StyleID
+    var darkRoad = L.tileLayer(mbStyleUrl, {id: 'mprommer/ckw043v138gry14qdqhs7opu1', token: apitoken,  attribution: mbAttr});
+    
+    var grayscale   = L.tileLayer(mbUrl, {id: 'mapbox.light', token: apitoken, attribution: mbAttr});
+    
+    var dark  = L.tileLayer(mbUrl, {id: 'mapbox.dark', token: apitoken,  attribution: mbAttr});
+    
+    var outdoors = L.tileLayer(mbUrl, {id: 'mapbox.outdoors', token: apitoken,  attribution: mbAttr});
+    
+    
+    //create the map*/
+    var map = L.map('map', {
+        center: [31.00, -5],//Coordinated to center the map
+        zoom: 6, //zoom level
+        layers:darkRoad //basemap
+    });
+    
+    //create the basemap control layer*/
+    var baseLayers = {
+		"Outdoors": outdoors,
+        "Grayscale": grayscale,
+		"Darkscale": dark,
+        "Dark Road": darkRoad
+    };
+    
+    L.control.layers(baseLayers).addTo(map);
+};
+
+//calling the function
+$(document).ready(createMap);
